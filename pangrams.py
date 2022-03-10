7de44f11564515a25a4f323f579941e5462c712c
Djinthc <99714269+Djinthc@users.noreply.github.com>
2022-03-10 10:00:34 +0100

    Update pangrams.py

--- pangrams.py
+++ pangrams.py
@@ -1,3 +1,5 @@
+# HackerRank preparation
+
 def pangrams(s):
     x = []
     pangram = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
